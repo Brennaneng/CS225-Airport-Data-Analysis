d30553d2c147ae433b0548d2027f7fc9bcdb25a2
Brennan Eng <bheng2@siebl-0224-12.ews.illinois.edu>
2021-04-13 21:51:17 -0500

    need to pull in new csv file

---
+++ COVIDMap/ReadFile.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+void read_file(){
+    //creating file pointer
+    fstream fin;
+
+    //opens file to be read
+    fin.open("all-states-history-death.csv", ios::in);
+}
