b93495d346f10ad5c5f87ffc927a5b8214afa8cc
Brennan Eng <bheng2@siebl-0224-12.ews.illinois.edu>
2021-04-13 23:01:40 -0500

    Everything committed by Brennan including readfile and tests

--- COVIDMap/ReadFile.cpp
+++ COVIDMap/ReadFile.cpp
@@ -1,10 +1,31 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-void read_file(){
+#include <vector>
+#include "ReadFile.h"
+using namespace std;
+vector<vector<string> > * read_file(){
     //creating file pointer
-    fstream fin;
+    ifstream file;
 
     //opens file to be read
-    fin.open("all-states-history-death.csv", ios::in);
+    file.open("all-states-history-death.csv");
+
+    //Created two vectors, TotalFile creates a vector of vectors that takes in each row called a vector datapoint
+    //datapoint creates a set of values enumerated by ',' which means each index of the datapoint will be date, state, and deaths
+    vector<vector<string> > * TotalFile;
+    vector<string> datapoint;
+    //reads while the file still has values to be read
+    while(file.good()){
+        //creates a temporary 
+        string line;
+        for(int count = 0; count < 3; count++){
+            getLine(file, line, ',');
+            datapoint.push_back(line); 
+        }
+        TotalFile->push_back(datapoint);
+        datapoint.clear();
+    }
+    cout<<"Done"<<endl;
+    return TotalFile;
 }
