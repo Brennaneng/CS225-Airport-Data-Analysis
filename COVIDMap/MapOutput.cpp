85352ae28c029555e49141424c05be7b16379998
Jorge Chavez <jorgejc2#illinois.edu>
2021-04-19 00:15:05 -0500

    added comments for what to do tomorrow

--- COVIDMap/MapOutput.cpp
+++ COVIDMap/MapOutput.cpp
@@ -2,12 +2,17 @@
 #include "TreeTraversals/PreorderTraversal.h"
 #include "TreeTraversals/InorderTraversal.h"
 
-int main() {
+//here is where we will create a map object. We will specify what kind of map where are working with meaning
+//that if we want a map for deaths, we create a map object called deaths. If we do a map for deaths, then we will
+//create a tree of states where each value is a vector full of deaths for each month. If we do a map for positive cases, 
+//then we create a tree of states where each value is a vector full of positive cases for each month
 
-    stateMap hello;
-    hello.printHello("ar", "az");
-    hello.printPeepeePoopoo();
+//map should have a constructor where it takes a csv file. it will then call the read in file function where 
+//a vector of total data is created. then that data will be used to create a binary tree
 
+//Private member: binary tree
+//when a binary tree is constructed, it will become the private variable for that map object
 
-    return 0;
-}
+//Private member: vector<PNG> mapPictures
+//each index will be a map for a certain date. we have to create a function that uses the binary tree to get data,
+//and then access this vector, and alter the png image for the corresponding month
