b7df3005e3e0f67e95987450b8c0e0add873e6ff
Brennan Eng <bheng2@siebl-0224-39.ews.illinois.edu>
2021-04-18 22:36:36 -0500

    Updated Makefile and needed to finish readfile

--- COVIDMap/MapOutput.cpp
+++ COVIDMap/MapOutput.cpp
@@ -1,12 +1,21 @@
 #include "MapTree.h"
 #include "TreeTraversals/PreorderTraversal.h"
 #include "TreeTraversals/InorderTraversal.h"
+#include "ReadFile.h"
+#include <vector>
+#include <iostream>
+using namespace std;
 
 int main() {
 
     stateMap hello;
     hello.printHello("ar", "az");
     hello.printPeepeePoopoo();
+    vector<vector<string> > * s = read_file();
+    for(unsigned int i = 0; i < s->size(); i++){
+        
+        cout << s[i] << endl;
+    }
 
 
     return 0;
--- COVIDMap/ReadFile.cpp
+++ COVIDMap/ReadFile.cpp
@@ -13,11 +13,11 @@ vector<vector<string> > * read_file(){
 
     //Created two vectors, TotalFile creates a vector of vectors that takes in each row called a vector datapoint
     //datapoint creates a set of values enumerated by ',' which means each index of the datapoint will be date, state, and deaths
-    vector<vector<string> > * TotalFile;
+    vector<vector<string>> * TotalFile;
     vector<string> datapoint;
     //reads while the file still has values to be read
     while(file.good()){
-        //creates a temporary 
+        //creates a temporary string called line to take each 
         string line;
         for(int count = 0; count < 3; count++){
             getLine(file, line, ',');
