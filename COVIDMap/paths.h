7d951c0662d64d474be5801a5bf077d49c913b6f
Brennan Eng <bheng2@siebl-0224-11.ews.illinois.edu>
2021-04-28 15:34:50 -0500

    completed statecoordinate vector

---
+++ COVIDMap/paths.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <utility>
+using namespace std;
+class paths{
+
+vector<pair<int,int>> coords();
+
+}
