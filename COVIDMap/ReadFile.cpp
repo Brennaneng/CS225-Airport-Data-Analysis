62cdda6f647d8f148dfd49b50876d75cb2b7abc8
Brennan Eng <bheng2@siebl-0224-38.ews.illinois.edu>
2021-04-29 16:15:59 -0500

    Readfile function

--- COVIDMap/ReadFile.cpp
+++ COVIDMap/ReadFile.cpp
@@ -1,58 +1,13 @@
  #include "ReadFile.h"
- 
-using namespace std;
+
 namespace functions {
-<<<<<<< HEAD
-vector<pair<int,int> > read_routes()  {
-    
-    //creating file pointer
-    ifstream file;
-
-    //opens file to be read
-    //Gives us Source airport ID,Destination airport ID
-    file.open("routes_filtered.csv");
-
-    //Creates a vector of pairs
-    vector<pair<int,int> > TotalFile;
-    //Creates pair
-    pair<int,int> datapair(0,0);
-    //creates a temporary string called line to take each cell
-    string line;
-    //counter to determine place in airport
-    int counter = 0;
-    //reads while the file still has values to be read
-    while(file.good()){
-        while(getline(file,line) ) {
-            stringstream ss(line);
-            counter = 0;
-            while (getline(ss,line,',') ) {
-                if(counter == 0) {
-                    int x = stoi(line);
-                    datapair.first = x;
-                }
-                else if(counter == 1) {
-                    int x = stoi(line);
-                    datapair.second = x;
-                }
-                counter++;
-            }
-            TotalFile.push_back(datapair);
-            datapair.first = 0;
-            datapair.second = 0;
-        }    
-    }
-    return TotalFile;
-}
-vector<vector<string> > read_file() {
-=======
 
 vector<vector<string>> read_file() {
->>>>>>> 84efd7bbd55a37a2248baa2b38f356181a735cbc
     //creating file pointer
     ifstream file;
 
     //opens file to be read
-    file.open("airports_filtered.csv");
+    file.open("airports_fitered.csv");
 
     //Created two vectors, TotalFile creates a vector of vectors that takes in each row called a vector datapoint
     //datapoint creates a set of values enumerated by ',' which means each index of the datapoint will be date, state, and deaths
@@ -61,31 +16,29 @@ vector<vector<string>> read_file() {
     //reads while the file still has values to be read
     while(file.good()){
         //creates a temporary string called line to take each 
-<<<<<<< HEAD
-        string line;
-        for(int count = 0; count < 3; count++){
-            getline(file, line, ',');
-            datapoint.push_back(line); 
-=======
         string datapoint;
-        for(int count = 0; count < 5; count++){
-            std::getline(file, datapoint, ',');
-            if(count == 2){
-                if(datapoint != 'United States'){
-                    line.clear();
-                    std::getline(file, datapoint, ','); //skips next two datapoints which are coordinates
-                    std::getline(file, datapoint, ',');
-                    continue; //continues onto next iteration which should be next line
-
-                }
+        while(getline(file,datapoint)){
+            std::stringstream ss(datapoint);
+            while(getline(ss,datapoint, ',')){
+                line.push_back(datapoint);
             }
-            line.push_back(datapoint); 
->>>>>>> 84efd7bbd55a37a2248baa2b38f356181a735cbc
+            if(line[2] == "United States"){
+                TotalFile.push_back(line);
+            }
+            line.clear();
         }
-        TotalFile.push_back(line);
-        line.clear();
+        // for(int count = 0; count < 5; count++){
+        //     std::getline(file, datapoint, ',');
+        //     line.push_back(datapoint); 
+        // }
+        // if(line[2] == "United States"){
+        //     TotalFile.push_back(line);
+        // }
+        // line.clear();
     }
 
+
+    cout<<"Done"<<endl;
     return TotalFile;
 };
 
@@ -100,16 +53,16 @@ vector<string> read_date(string full_date) {
     return dates;
 };
 int returnDateIndex(string month, string year){
-    int x = std::stoi(month); 
-    --x;
-    if(year == "2020"){
-        return x;
-    }
-    else{
-    x = x + 12;
+int x = stoi(month); 
+--x;
+if(year == "2020"){
     return x;
-    }
-    return -1;
+}
+else{
+x = x + 12;
+return x;
+}
+return -1;
 }
 void clean_data() {
     
