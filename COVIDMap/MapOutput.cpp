551d8c0bcfca0a3a62dfa08ecef80e04c5b20e05
Brennan Eng <bheng2@siebl-0224-38.ews.illinois.edu>
2021-04-29 18:43:53 -0500

    added Eulers path

--- COVIDMap/MapOutput.cpp
+++ COVIDMap/MapOutput.cpp
@@ -77,7 +77,7 @@ Map::Map(vector<vector<string>> file, vector<pair<int,int>> routeFile){
     readRoutes(routeFile);
 
     //this sets up our AVL Tree
-    for(int i = 0; i < routes_.size(); i++){
+    for(unsigned int i = 0; i < routes_.size(); i++){
         pair<int,int> curr = routes_[i];
         vector<vector<double>> firstValue = _tree->find(curr.first);
         vector<vector<double>> secondValue = _tree->find(curr.second);
@@ -113,8 +113,53 @@ vector<pair<int,int>> Map::printRoutes(){
 
 
 
+double Map::toRadians(const double degree)
+{
+    // cmath library in C++
+    // defines the constant
+    // M_PI as the value of
+    // pi accurate to 1e-30
+    double one_deg = (M_PI) / 180;
+    return (one_deg * degree);
+}
+ 
+double Map::Eulerpath(double lat1, double long1, double lat2, double long2){
+{
+    // Convert the latitudes
+    // and longitudes
+    // from degree to radians.
+    lat1 = toRadians(lat1);
+    long1 = toRadians(long1);
+    lat2 = toRadians(lat2);
+    long2 = toRadians(long2);
+     
+    // Haversine Formula
+    double dlong = long2 - long1;
+    double dlat = lat2 - lat1;
+ 
+    double ans = pow(sin(dlat / 2), 2) +
+                          cos(lat1) * cos(lat2) *
+                          pow(sin(dlong / 2), 2);
+ 
+    ans = 2 * asin(sqrt(ans));
+ 
+    // Radius of Earth in
+    // Kilometers, R = 6371
+    // Use R = 3956 for miles
+    double R = 6371;
+     
+    // Calculate the result
+    ans = ans * R;
+ 
+    return ans;
+}
+
+
+
 // }
-void Map::insertStates(){
+/*
+void Map::insertStates()
+{
 States = new vector<string> ({"AK",
 "AL",
 "AR",
@@ -165,4 +210,6 @@ States = new vector<string> ({"AK",
 "WI",
 "WV",
 "WY"});
-};
+}
+};*/
+}
