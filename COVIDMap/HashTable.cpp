73ee522b8c86edf4af7ad5a373cc4063219c623f
Miguel Moreno <miguel5@linux-39.ews.illinois.edu>
2021-05-06 16:12:07 -0500

    Hashes

---
+++ COVIDMap/HashTable.cpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "HashTable.h"
+
+using namespace std;
+
+HashTable::hash() {
+    for (int i = 0; i < tableSize; i++) {
+        HashTable[i] = new MapNode;
+    }
+}
+
+int HashTable::hash(int key) {
+    int 
+}
+//hash helper functions since we are double hashing
+int HashTable::hash1(string key){
+
+}
+int HashTable::hash2(string key){
+    
+}
+
+void HashTable::addMapNode(int id) {
+    int index = hash(ID);
+
+    // Finish making the MapNode
+    HashTable[index]->key = id;
+}
+void HashTable::printTable() {
+    //Finish displaying all the other data from each airport
+    for (int i = 0; i < tableSize; i++) {
+        cout <<"----------------------\n";
+        cout <<"Index = "<< i << endl;
+        cout <<"Airport ID = "<< HashTable[i]->key << endl;
+        cout <<"Airport Name = "<< HashTable[i]->value << endl;
+        cout <<"Longitude/Latitude "<< HashTable[i]->key << endl;
+        cout <<"----------------------\n";
+
+
+    }
+}
+void removeMapNode(int id) {
+    int index = hash(id);
+    MapNode* delPtr;
+    if(HashTable[i]->key != 0 && HashTable[i]->value != "empty") {
+        HashTable[i]->key = 0;
+        HashTable[i]->value = "empty";
+        HashTable[i]->currentWeight = 0;
+    }
+}
